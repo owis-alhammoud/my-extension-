060abcc35cc87a5a82fd000ee47a886ed4fd0eef
owis-alhammoud <alhammoudowis@gmail.com>
2025-07-24 15:21:04 +0300

    Open PGN popup window

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 (function() {
   const moves = [];
   let pgn = '';
+  let popupWin;
 
   function movesToPgn(list) {
     let result = '';
@@ -18,6 +19,16 @@
     chrome.storage.local.set({ moves, pgn });
     chrome.runtime.sendMessage({ pgn, moves: [...moves] });
   };
+
+  function openPopup() {
+    if (!popupWin || popupWin.closed) {
+      popupWin = window.open(
+        chrome.runtime.getURL('popup.html'),
+        'lichessPGN',
+        'width=300,height=400'
+      );
+    }
+  }
 
   function getMovesContainer() {
     return document.querySelector('.vertical-move-list') ||
@@ -46,6 +57,7 @@
     captureMoves(container);
     const observer = new MutationObserver(() => captureMoves(container));
     observer.observe(container, { childList: true, subtree: true });
+    openPopup();
   }
 
   if (document.readyState === 'complete' || document.readyState === 'interactive') {
